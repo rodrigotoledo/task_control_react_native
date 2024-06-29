84a3c5a9e2651142ae0f9a0fca87d0402b53ea6a
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-06-29 19:30:59 -0300

    feat: Update Android minSdkVersion, ndkVersion, and kotlinVersion

--- hooks/useRefreshOnFocus.jsx
+++ hooks/useRefreshOnFocus.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import {useFocusEffect} from '@react-navigation/native';
 
 export function useRefreshOnFocus(refetch) {
-  const firstTimeRef = React.useRef(true);
-
   useFocusEffect(
     React.useCallback(() => {
-      if (firstTimeRef.current) {
-        firstTimeRef.current = false;
-        return;
-      }
-
       refetch();
     }, [refetch]),
   );
